33da98323ae8e2dd08623b707d8af643d6cff375
Slayer Viper <44239367+SlayerViper@users.noreply.github.com>
2021-03-31 17:56:50 +0300

    Create load.lua

---
+++ load.lua
@@ -0,0 +1,23 @@
+local player = game.Players.LocalPlayer
+local whitelist = {
+   ["UserId"] = 1677188092;
+   ["GroupId"] = 1782387;
+}
+
+local enable = false
+
+for i, v in pairs(whitelist) do
+   if i == "UserId" then
+      if player.UserId == v then
+         enable = true
+      end
+   elseif i == "GroupId" then
+      if player:IsInGroup(v) then
+        enable = true
+      end
+   end
+end
+
+if enable then
+   LocalPlayer.PlayerGui:FindFirstChild("RadioComms").Enabled = true
+end
