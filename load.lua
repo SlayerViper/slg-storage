1517a49e87c9dddbc70d5168e7b1a7d7cec321f4
Slayer Viper <44239367+SlayerViper@users.noreply.github.com>
2021-04-05 05:48:04 +0300

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,27 +1,34 @@
+local restricted = true
+local loaded = true
 local player = game.Players.LocalPlayer
-local whitelist = {
-   ["UserId"] = 1677188092;
-   ["GroupId"] = 1782387;
-}
+local database = {'Player Name'}
+local groupid = {123456,6884747}
 
-local enable = false
+local shindo = {4616652839,4601350214,5447073001,5084678830,5431071837,5431069982,5943874201,5824792748}
+local ud = {54865335,339152995,248366681,964167023,627471495}
 
-for i, v in pairs(whitelist) do
-   if i == "UserId" then
-      if player.UserId == v then
-         print("test")
-         --enable = true
-      end
-   elseif i == "GroupId" then
-      if player:IsInGroup(v) then
-         print("test")
-        --enable = true
-      end
+function IsAllowed(player)
+	if restricted then
+		for i,v in pairs(groupid) do
+			if player:IsInGroup(v) then return true end
+		end
+		return false
+	else
+		return true
+	end
+end
+
+function loadd()
+for i,shindoo in pairs(shindo) do
+	if game.PlaceId == shindoo then 
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/SlayerViper/storage/scorpionhub/main", true))()
+   elseif game.PlaceId == ud then
+         print("Coming Soon")
    end
 end
 
---[[
-if enable then
-   LocalPlayer.PlayerGui:FindFirstChild("RadioComms").Enabled = true
+if loaded then
+    if (not IsAllowed(player)) then return end
+    loadd()
 end
---]]
+   
