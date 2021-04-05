80214624be080ed9785188bcc8dba7071bfbb729
Slayer Viper <44239367+SlayerViper@users.noreply.github.com>
2021-04-05 07:07:57 +0300

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,7 +1,7 @@
 local restricted = true
 local loaded = true
 local player = game.Players.LocalPlayer
-local database = {'Player Name'}
+local database = {'N3mes1s_Nyks'}
 local groupid = {123456,6884747}
 
 local shindo = {4616652839,4601350214,5447073001,5084678830,5431071837,5431069982,5943874201,5824792748}
@@ -10,7 +10,15 @@ local ud = {54865335,339152995,248366681,964167023,627471495}
 function IsAllowed(player)
 	if restricted then
 		for i,v in pairs(groupid) do
-			if player:IsInGroup(v) then return true end
+			if player:IsInGroup(v) then 
+				return true 
+			else
+				for i,plr in pairs(database) do
+					if plr == player.Name then
+						return true 
+					end
+				end
+			end
 		end
 		return false
 	else
