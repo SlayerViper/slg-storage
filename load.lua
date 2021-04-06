3eac31607cdae6bc67fe08654baf6f5c5e19a471
Slayer Viper <44239367+SlayerViper@users.noreply.github.com>
2021-04-06 06:40:21 +0300

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,5 +1,3 @@
-local restricted = true
-local loaded = true
 local player = game.Players.LocalPlayer
 local database = {'N3mes1s_Nyks','franklin19415'}
 local groupid = {123456,6884747}
@@ -10,17 +8,15 @@ local ud = {54865335,339152995,248366681,964167023,627471495}
 local nTitle
 local nText
 
-function startLoad()
-	for i,v in pairs(groupid) do
-		if player:IsInGroup(v) then 
-			guiLoader()
-		else
-			for i,plr in pairs(database) do
-				if plr == player.Name then
-					guiLoader()
-				else
-					nTitle = "Whitelist Notify" nText = "You not Whitelisted!" SendNotify()
-				end
+function startLoad(nPlayer)
+	for _,group in pairs(groupid) do
+		for _,plr in pairs(database) do
+			if player:IsInGroup(group) then 
+				guiLoader()
+			elseif plr == player.Name then
+				guiLoader()
+			else
+				nTitle = "Whitelist Notify" nText = "You not Whitelisted!" SendNotify()
 			end
 		end
 	end
@@ -28,18 +24,16 @@ end
 
 function guiLoader()
     for i,shindoo in pairs(shindo) do
-	if game.PlaceId == shindoo then 
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/SlayerViper/storage/scorpionhub/main.lua", true))()
-	else
-		for i,udd in pairs(ud) do
-			if game.PlaceId == udd then
+    	for i,udd in pairs(ud) do
+			if game.PlaceId == shindoo then 
+				loadstring(game:HttpGet("https://raw.githubusercontent.com/SlayerViper/storage/scorpionhub/main.lua", true))()
+			elseif game.PlaceId == udd then
 				loadstring(game:HttpGet("https://raw.githubusercontent.com/SlayerViper/storage/scorpionhubforud/main.lua", true))()
+			else
+				nTitle = "Game List Notify" nText = "This game has not been added to the list!" SendNotify()
 			end
 		end
-	else
-		nTitle = "Game List Notify" nText = "This game has not been added to the list!" SendNotify()
 	end
-    end
 end
 
 function SendNotify()
@@ -50,6 +44,5 @@ function SendNotify()
 	     	Duration = 5;
 	})
 end
-	
+
 startLoad()
-   
